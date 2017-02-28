9b101b0ae0da74f0ee500d3e46a2f3d9b2b84ea9
bunkermaster <yann.le-scouarnec@hetic.net>
2017-02-28 10:19:21 +0100

    First commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+var_dump($_SERVER);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+</head>
+<body role="document">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">WtfWeb</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Teletubbies</a></li>
+                <li class="active"><a href="kittens.php">Kittens</a></li>
+                <li><a href="ironmaiden.php">Iron Maiden</a></li>
+                <li><a href="16horsepower.php">16 Horse power</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container theme-showcase" role="main">
---
+++ kittens.php
@@ -0,0 +1,6 @@
+<?php require "header.php";?>
+        <div class="jumbotron">
+            <h1>Les Chatons!</h1>
+            <p>C'est mignon.</p>
+        </div>
+        <img class="img-thumbnail" alt="Kittens" src="img/three_kittens.jpg" data-holder-rendered="true">
