3170e3e020423b38e90f89c9edfd6a15e599b5b9
bunkermaster <yann.le-scouarnec@hetic.net>
2017-04-25 12:23:00 +0200

    Yolo l'admin

---
+++ admin/index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yann Le Scouarnec <bunkermaster@gmail.com>
+ * Date: 25/04/2017
+ * Time: 11:36
+ */
+//require_once "../connect.php";
+require_once dirname(__DIR__)."/connect.php";
+// requete de recuperation du compteur de pages en base
+$sql = "SELECT count(*) as compteur FROM `page`;";
+// preparation du compteur
+$stmt = $pdo->prepare($sql);
+// execution
+$stmt->execute();
+// test
+if ($stmt->errorCode() != '00000') {
+    die($stmt->errorInfo()[2]);
+}
+// recuperation de la valeur du compteur
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+var_dump($row); die();
+// $count contient le nombre d'enregistrements en base
+$count = $row['compteur'];
+// recuperation des pages
+$sql = "SELECT `id`, `slug`, `nav_title`, `H1`, `paragraphe`, `img`, `alt` FROM `page`;";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+if ($stmt->errorCode() != '00000') {
+    die($stmt->errorInfo()[2]);
+}
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Liste des pages</title>
+</head>
+<body>
+<h1>Liste des pages</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Slug</th>
+        <th>Action</th>
+    </tr>
+    <?php if ($count > 0) :?>
+    <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+    <tr>
+        <td><?= $row['id']?></td>
+        <td><?php echo $row['slug']?></td>
+        <td>n/a</td>
+    </tr>
+    <?php endwhile; ?>
+    <?php else:?>
+        <td colspan="3">Pas de pages</td>
+    <?php endif;?>
+</table>
+</body>
+</html>
